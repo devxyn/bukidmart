e87b1ccba071576acc80dd581827aee139caa9f7
devxyn <johnlawrencehermo@gmail.com>
2024-04-15 17:02:38 +0800

    fix: connected sign up to the database

--- server/controllers/signUpController.js
+++ server/controllers/signUpController.js
@@ -1,14 +1,23 @@
-const signUpController = (req, res) => {
-  const { email, password } = req.body;
+import User from '../models/User.js';
+
+const signUpController = async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
 
   try {
+    const user = await User.create({
+      email,
+      password,
+      firstName,
+      lastName,
+    });
+
     res.status(201).json({
       message: 'Signup Successfully',
-      user: { email, password },
+      user: { email: user.email, password: user.password, firstName: user.firstName, lastName: user.lastName },
     });
   } catch (error) {
     res.status(500).json({
-      message: 'Something went wrong',
+      message: 'Failed to sign up.',
     });
   }
 };
