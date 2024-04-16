35c1b539f50fdac8c235ffe22b0271046db1f41c
devxyn <johnlawrencehermo@gmail.com>
2024-04-16 17:57:03 +0800

    feat: updated the user schema

--- server/models/User.js
+++ server/models/User.js
@@ -3,15 +3,25 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-  email: { type: String, required: true, unique: true, trim: true },
-  password: { type: String, required: true, trim: true },
-  firstName: { type: String, required: true, trim: true },
-  lastName: { type: String, required: true, trim: true },
-  address: { type: String, trim: true },
-  phoneNumber: { type: String, trim: true },
-  role: { type: String, enum: ['customer', 'seller'], default: 'customer' },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  contactNumber: { type: String, default: '' },
+  address: {
+    addressLine: { type: String, default: '' },
+    city: { type: String, default: '' },
+    state: { type: String, default: '' },
+    zipCode: { type: String, default: '' },
+  },
+  cart: [
+    {
+      product: { type: Schema.Types.ObjectId, ref: 'Product' },
+      quantity: { type: Number },
+    },
+  ],
+  orders: [{ type: Schema.Types.ObjectId, ref: 'Order' }],
   isAdmin: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
 });
 
 const User = model('User', userSchema);
