f149c80b6003de546415e50c18f9047f2014f534
devxyn <johnlawrencehermo@gmail.com>
2024-05-16 23:37:25 +0800

    fix: fixed footer in mobile view

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,14 @@
 const Footer = () => {
   return (
-    <footer className='h-14 bg-primary flex justify-center items-center'>
-      <p className='text-white text-md font-base'>
+    <footer className='h-20 md:h-14 bg-primary flex flex-col md:flex-row justify-center items-center'>
+      <p className='text-white text-md font-base md:mr-1'>
         Developed by{' '}
         <a className='font-bold' href='https://github.com/devxyn/' target='_blank'>
           devxyn
-        </a>{' '}
-        | &copy; 2024 BukidMart. All rights reserved.
+        </a>
+        {''}
       </p>
+      <p className='text-white text-md font-base'> &copy; 2024 BukidMart. All rights reserved.</p>
     </footer>
   );
 };
