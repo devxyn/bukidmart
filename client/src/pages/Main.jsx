86ad3f3f2e1ffdd2cae966f15f82adf438119947
devxyn <johnlawrencehermo@gmail.com>
2024-05-16 17:41:05 +0800

    feat: created statement section

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,11 +1,13 @@
 import Banner from '../components/Banner';
 import FeaturedProduct from '../components/FeaturedProduct';
+import StatementSection from '../components/StatementSection';
 
 const Main = () => {
   return (
     <>
       <Banner />
       <FeaturedProduct />
+      <StatementSection />
     </>
   );
 };
