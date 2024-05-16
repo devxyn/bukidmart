0302650a86e02614f5c6c4cb4181c47a2447ff44
devxyn <johnlawrencehermo@gmail.com>
2024-05-16 13:28:10 +0800

    fix: fixed elements arrangement and font sizes

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -2,14 +2,16 @@ import banner from '../assets/banner.png';
 
 const Banner = () => {
   return (
-    <div className='bg-backdrop h-[50vh] md:h-[90vh] px-20 relative flex items-center justify-center'>
-      <div className='text-center md:text-left z-10'>
-        <h1 className='text-4xl font-semibold tracking-wide md:tracking-widest leading-tight mb-10'>
-          <span className='text-secondary'>Farm-fresh</span> goodness delivered to your doorstep
+    <div className='bg-backdrop h-[50vh] md:h-[90vh] px-5 md:px-20 relative flex items-center justify-center'>
+      <div className='text-left z-10'>
+        <h1 className='text-4xl md:text-5xl font-semibold tracking-wide md:tracking-widest leading-tight mb-10'>
+          <span className='text-secondary font-bold'>Farm-fresh</span> goodness delivered to your doorstep
         </h1>
-        <div className='flex gap-4'>
-          <button className='bg-accent text-white text-sm font-medium px-6 py-2 rounded-xl'>Shop now</button>
-          <button className='border border-primary text-primary text-sm font-medium px-6 py-2 rounded-xl'>
+        <div className='flex justify-center md:justify-start gap-4'>
+          <button className='bg-accent text-white text-base md:text-2xl font-semibold px-8 py-3 md:px-10 md:py-3 rounded-xl'>
+            Shop now
+          </button>
+          <button className='border border-primary text-primary text-base md:text-2xl font-semibold px-8 py-3 md:px-10 md:py-3 rounded-xl'>
             Learn more
           </button>
         </div>
