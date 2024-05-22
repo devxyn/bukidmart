dddeb09a96719295d0bb0b611b1a376bdbda67e8
devxyn <johnlawrencehermo@gmail.com>
2024-05-22 12:34:20 +0800

    fix: fixed the product card in featured product section

--- client/src/components/FeaturedProduct.jsx
+++ client/src/components/FeaturedProduct.jsx
@@ -1,16 +1,18 @@
+/* eslint-disable react/prop-types */
 import ProductCard from './ProductCard';
 
-const FeaturedProduct = () => {
+const FeaturedProduct = ({ productData }) => {
+  const featuredData = productData.filter((product) => product.isFeatured === true);
+
   return (
-    <section className='h-[80%] md:h-screen flex flex-col items-center justify-center gap-10 md:gap-14 px-5 py-10'>
-      <h2 className='text-2xl md:text-5xl tracking-widest'>FEATURED PRODUCTS</h2>
-      <div className='flex w-full md:justify-center overflow-x-scroll md:overflow-x-auto gap-5 md:gap-10 scroll-smooth'>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+    <section className='h-[80%] lg:h-screen flex flex-col items-center justify-center gap-10 lg:gap-14 px-5 py-10'>
+      <h2 className='text-2xl lg:text-5xl tracking-widest'>FEATURED PRODUCTS</h2>
+      <div className='flex w-full lg:justify-center overflow-x-scroll lg:overflow-x-auto gap-5 lg:gap-10 scroll-smooth'>
+        {featuredData.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
       </div>
-      <button className='bg-accent text-white text-lg md:text-2xl font-normal px-6 py-3 md:px-10 md:py-4 rounded-xl'>
+      <button className='bg-accent text-white text-lg lg:text-2xl font-normal px-6 py-3 lg:px-10 lg:py-4 rounded-xl'>
         VIEW PRODUCTS
       </button>
     </section>
