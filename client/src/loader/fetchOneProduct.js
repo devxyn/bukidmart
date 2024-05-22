07d391b92db09d2ac8e3e653bf82b5e7bfeffb8c
devxyn <johnlawrencehermo@gmail.com>
2024-05-22 16:05:58 +0800

    fix: removed console.logs

--- client/src/loader/fetchOneProduct.js
+++ client/src/loader/fetchOneProduct.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const fetchOneProduct = async ({ params }) => {
   try {
     const response = await axios.get(`http://localhost:4000/api/products/${params.id}`);
-    console.log(response.data.product);
     return response.data.product;
   } catch (error) {
     console.error(error);
