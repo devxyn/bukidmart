02358451acae7f9093d40ae259944b3f82242842
devxyn <johnlawrencehermo@gmail.com>
2024-05-23 18:12:33 +0800

    feat: added testimonials data

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,13 +1,14 @@
 import TestimonialCards from './TestimonialCards';
+import { testimonials } from '../data/testimonials';
 
 const Testimonials = () => {
   return (
     <section className='h-auto lg:h-screen flex flex-col items-center justify-center gap-10 lg:gap-20 px-5 py-10'>
       <h2 className='text-3xl lg:text-5xl tracking-widest'>TESTIMONIALS</h2>
       <div className='flex flex-col lg:flex-row justify-center gap-5 lg:gap-20'>
-        <TestimonialCards />
-        <TestimonialCards />
-        <TestimonialCards />
+        {testimonials.map((testimony) => (
+          <TestimonialCards key={testimony.name} testimony={testimony} />
+        ))}
       </div>
     </section>
   );
---
+++ client/src/data/testimonials.jsx
@@ -0,0 +1,24 @@
+import shiela from '../assets/shiela.jpg';
+import frio from '../assets/frio.jpg';
+import leny from '../assets/leny.jpg';
+
+export const testimonials = [
+  {
+    name: 'Shiela P.',
+    message:
+      "I've been a loyal customer of BukidMart for over a year now, and I can't imagine going back to buying groceries any other way. The freshness of their produce is unmatched, and the convenience of having it delivered to my doorstep saves me so much time. Plus, knowing that I'm supporting local farmers makes the experience even better!",
+    image: shiela,
+  },
+  {
+    name: 'Leny R.',
+    message:
+      'As someone who values healthy eating and supporting local agriculture, BukidMart has become my go-to for all my grocery needs. Their commitment to providing fresh, organic produce is evident in every order I receive. I love being able to trust where my food comes from, and BukidMart never disappoints.',
+    image: leny,
+  },
+  {
+    name: 'Joshua F.',
+    message:
+      "BukidMart has truly transformed my shopping routine. The ease of receiving fresh farm produce at my door is unmatched. Knowing that I'm supporting local farmers while receiving exceptional customer service adds significant value to each purchase. BukidMart has earned my loyalty through their reliable service and commitment to quality.",
+    image: frio,
+  },
+];
