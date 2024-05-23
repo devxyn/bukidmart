0811d8f4d6012bbc4a0f044733df330c9128f2f5
devxyn <johnlawrencehermo@gmail.com>
2024-05-23 17:43:33 +0800

    feat: connected the add to cart button to the backend

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,9 +1,39 @@
 import cocopandan from '../assets/coco pandan.png';
 import { HiArrowLongLeft, HiOutlineShoppingBag } from 'react-icons/hi2';
 import { Link, useLoaderData } from 'react-router-dom';
+import useGetUserID from '../hooks/useGetUserID';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Product = () => {
   const product = useLoaderData();
+  const userID = useGetUserID();
+
+  const [cartItem, setCartItem] = useState({ userID, product: product._id, quantity: 1 });
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const btnMessage = isSuccess ? 'Product added to cart!' : 'Add to cart';
+
+  const handleAddToCart = async () => {
+    try {
+      const response = await axios.put('http://localhost:4000/api/cart/add', cartItem);
+      if (response.status === 201) setIsSuccess(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addQuantity = () => {
+    setCartItem((prevState) => ({ ...prevState, quantity: Math.min(prevState.quantity + 1, 50) }));
+  };
+  const subtractQuantity = () => {
+    setCartItem((prevState) => ({ ...prevState, quantity: Math.max(prevState.quantity - 1, 1) }));
+  };
+
+  const handleInputChange = (e) => {
+    const value = Number(e.target.value);
+    if (value >= 0 && value <= 50) setCartItem({ ...cartItem, quantity: value });
+  };
 
   return (
     <main className='h-full pt-[100px] lg:p-40 lg:pb-20 lg:pt-[160px] bg-backdrop'>
@@ -26,18 +56,33 @@ const Product = () => {
               <div className='flex items-center justify-between gap-5'>
                 <p>Quantity</p>
                 <div className='flex justify-center items-center border border-black '>
-                  <button className=' px-3 py-1'>-</button>
-                  <input type='number' min={0} max={25} className='w-14 text-center border border-x-black py-1' />
-                  <button className='border px-3 py-1'>+</button>
+                  <button className=' px-3 py-1' onClick={subtractQuantity}>
+                    -
+                  </button>
+                  <input
+                    type='number'
+                    value={cartItem.quantity}
+                    name='quantity'
+                    onChange={handleInputChange}
+                    className='w-14 text-center border border-x-black py-1'
+                  />
+                  <button className='border px-3 py-1' onClick={addQuantity}>
+                    +
+                  </button>
                 </div>
               </div>
               {/* <p className='text-right lg:text-left text-placeholder text-sm'>{product?.quantity} kg(s) available </p> */}
             </div>
           </div>
 
-          <button className='w-full lg:w-auto flex items-center justify-center gap-3 text-lg bg-accent text-white px-5 py-3 rounded-btn'>
-            <HiOutlineShoppingBag size={32} className='stroke-white' />
-            Add to cart
+          <button
+            onClick={handleAddToCart}
+            className={`w-full lg:w-auto flex items-center justify-center gap-3 px-5 py-3 rounded-btn ${
+              isSuccess ? 'bg-transparent border-2 border-accent' : 'bg-accent'
+            }`}
+            disabled={isSuccess}>
+            <HiOutlineShoppingBag size={32} className={isSuccess ? 'stroke-accent' : 'stroke-white'} />
+            <p className={`text-lg ${isSuccess ? 'text-accent font-bold' : 'text-white'}`}> {btnMessage}</p>
           </button>
         </div>
       </div>
