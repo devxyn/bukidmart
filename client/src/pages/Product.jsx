5d2ffa98739d4e80e095cb0df3bfe3817e230129
devxyn <johnlawrencehermo@gmail.com>
2024-05-23 15:44:07 +0800

    fix: fixed price element styling

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -18,7 +18,9 @@ const Product = () => {
         <div className='p-5 lg:p-10'>
           <div className='flex flex-col gap-2 mb-10'>
             <h3 className='text-4xl font-semibold'>{product?.name}</h3>
-            <p className='text-xl font-semibold text-primary'>₱{product?.price.toLocaleString('en-US')}</p>
+            <p className='text-xl font-semibold text-primary'>
+              ₱{product?.price.toLocaleString('en-US')} <span className='font-light text-lg'>(1 kg)</span>{' '}
+            </p>
             <p className='text-sm mb-10 text-placeholder'>{product.description}</p>
             <div className='flex flex-col lg:flex-row lg:items-center w-full gap-2 lg:gap-5'>
               <div className='flex items-center justify-between gap-5'>
@@ -29,7 +31,7 @@ const Product = () => {
                   <button className='border px-3 py-1'>+</button>
                 </div>
               </div>
-              <p className='text-right lg:text-left text-placeholder text-sm'>{product?.quantity} kg(s) available </p>
+              {/* <p className='text-right lg:text-left text-placeholder text-sm'>{product?.quantity} kg(s) available </p> */}
             </div>
           </div>
 
