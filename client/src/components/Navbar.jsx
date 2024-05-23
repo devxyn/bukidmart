490ace579faba5b6210151a936df1d4fe9767495
devxyn <johnlawrencehermo@gmail.com>
2024-05-23 18:37:45 +0800

    feat: added nav menu for mobile preview

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,24 +1,67 @@
-import { HiBars3, HiOutlineMagnifyingGlass, HiOutlineShoppingBag } from 'react-icons/hi2';
+import { HiBars3, HiOutlineMagnifyingGlass, HiOutlineShoppingBag, HiOutlineXCircle } from 'react-icons/hi2';
 import { Link, useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
+import { useState } from 'react';
 
 const Navbar = () => {
   const [cookies, setCookies] = useCookies(['access_token']);
   const navigate = useNavigate();
+  const [showNav, setShowNav] = useState(false);
 
   const handleLogout = () => {
     setCookies('access_token', '');
     window.localStorage.clear();
     navigate('/login');
+    setShowNav(false);
   };
 
   return (
     <nav className='fixed z-50 h-[100px] lg:h-[80px] w-full bg-primary shadow-md'>
       {/* {Mobile Nav bar} */}
+      {showNav && (
+        <div className='lg:hidden absolute bg-primary w-full h-screen z-[100] flex flex-col items-center justify-center'>
+          <button className='absolute top-4 right-4' onClick={() => setShowNav(false)}>
+            <HiOutlineXCircle size={40} className='stroke-white' />
+          </button>
+          <Link
+            onClick={() => setShowNav(false)}
+            className='border border-white w-full text-center text-white py-4 text-lg font-semibold'
+            to='/'>
+            HOME
+          </Link>
+          <Link
+            onClick={() => setShowNav(false)}
+            className='border border-white w-full text-center text-white py-4 text-lg font-semibold'
+            to='/'>
+            ABOUT US
+          </Link>
+          <Link
+            onClick={() => setShowNav(false)}
+            className='border border-white w-full text-center text-white py-4 text-lg font-semibold'
+            to='/products'>
+            PRODUCTS
+          </Link>
+          <Link
+            onClick={() => setShowNav(false)}
+            className='border border-white w-full text-center text-white py-4 text-lg font-semibold'
+            to='/products'>
+            SHOP NOW
+          </Link>
+          {cookies.access_token && (
+            <button
+              className='border border-white w-full text-center text-white py-4 text-lg font-semibold'
+              onClick={handleLogout}>
+              LOG OUT
+            </button>
+          )}
+        </div>
+      )}
       <div className='h-full w-full flex flex-col gap-2 items-center justify-center lg:hidden'>
         <div className='w-full flex justify-between px-5 items-center'>
           <div className='flex gap-1'>
-            <HiBars3 size={32} className='fill-white' />
+            <button onClick={() => setShowNav(true)}>
+              <HiBars3 size={32} className='fill-white' />
+            </button>
             <Link to='/' className='text-white text-2xl'>
               BukidMart
             </Link>
@@ -73,11 +116,13 @@ const Navbar = () => {
             </Link>
           </div>
           <div className='flex gap-4 items-center'>
-            <button
-              className='text-white text-base font-semibold px-4 py-3 rounded-lg border-2 border-white cursor-pointer'
-              onClick={handleLogout}>
-              {cookies.access_token ? 'Log out' : 'Sign in'}
-            </button>
+            {cookies.access_token && (
+              <button
+                className='text-white text-base font-semibold px-4 py-3 rounded-lg border-2 border-white cursor-pointer'
+                onClick={handleLogout}>
+                Log Out
+              </button>
+            )}
             <Link to={cookies.access_token ? '/cart' : '/login'}>
               <HiOutlineShoppingBag size={32} className='stroke-white cursor-pointer' />
             </Link>
