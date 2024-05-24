2daab3b6f835e4b97cb1ed3c0e4466d359c633d2
devxyn <johnlawrencehermo@gmail.com>
2024-05-24 15:38:23 +0800

    feat: added a delete button in cart item

--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -1,9 +1,18 @@
 /* eslint-disable react/prop-types */
 import cocopandan from '../assets/coco pandan.png';
+import { HiOutlineTrash } from 'react-icons/hi2';
 
-const CartItem = ({ item, imgHeight }) => {
+const CartItem = ({ item, imgHeight, isDisplayed, removeItem }) => {
   return (
-    <div className='flex w-full h-full'>
+    <div className='flex w-full h-full relative'>
+      {isDisplayed && (
+        <button
+          className='absolute top-8 right-5 lg:top-28 lg:right-6 flex flex-row items-center gap-1 text-red-500 font-semibold'
+          onClick={removeItem}>
+          <HiOutlineTrash size={28} className='hidden lg:block stroke-red-500' />
+          Remove
+        </button>
+      )}
       <img className={`h-20 md:${imgHeight}`} src={cocopandan} alt='coco pandan' />
       <div className='w-full flex flex-col md:flex-row md:justify-between md:items-center px-3 md:px-5 '>
         <div className='md:w-1/2 flex flex-col'>
