ec1a0923786b57bef4dc7a26a278d838ca65ad1f
devxyn <johnlawrencehermo@gmail.com>
2024-05-30 19:29:21 +0800

    feat: replaced the api url back to localhost

--- client/src/loaders/fetchAllOrders.js
+++ client/src/loaders/fetchAllOrders.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-const fetchAllProducts = async () => {
+const fetchAllOrders = async () => {
   try {
     const response = await axios.get('http://localhost:4000/api/orders/');
+    console.log(response.data.orders);
     return response.data.orders;
   } catch (error) {
     console.error(error);
   }
 };
 
-export default fetchAllProducts;
+export default fetchAllOrders;
