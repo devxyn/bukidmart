e0da3d9d4369c255a01c7d03cc1f00d2737fdb66
devxyn <johnlawrencehermo@gmail.com>
2024-05-31 17:55:15 +0800

    feat: created a get route for getting single order

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -13,4 +13,15 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const order = await Order.findById(id);
+
+    res.status(200).json({ order });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong!' });
+  }
+});
+
 export { router as orderRoutes };
