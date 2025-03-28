b3b5768265abe9850979ed3e38e17166c8dc6132
devxyn <johnlawrencehermo@gmail.com>
2025-03-28 16:17:23 +0800

    feat: update product routes to include authentication middleware for adding, modifying, and deleting products

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { verifyToken, isAdmin } from '../middlewares/authMiddleware.js';
 import {
   addProduct,
   deleteProduct,
@@ -9,10 +10,10 @@ import {
 
 const router = new Router();
 
-router.post('/add', addProduct);
 router.get('/', getAllProducts);
+router.post('/', verifyToken, isAdmin, addProduct);
 router.get('/:id', getProduct);
-router.patch('/modify', modifyProduct);
-router.delete('/delete/:id', deleteProduct);
+router.put('/:id', verifyToken, isAdmin, modifyProduct);
+router.delete('/:id', verifyToken, isAdmin, deleteProduct);
 
-export { router as productRoutes };
+export default router;
