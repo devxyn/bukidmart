23c776577bdaf64038a01e70b231406702834f5b
devxyn <johnlawrencehermo@gmail.com>
2025-03-28 16:18:01 +0800

    feat: integrate product and cart routes into the server for enhanced API functionality

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/database.js';
 import authRoutes from './routes/authRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import cartRoutes from './routes/cartRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,6 +18,8 @@ app.get('/api', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/cart', cartRoutes);
 
 app.listen(port, () => {
   connectDB();
