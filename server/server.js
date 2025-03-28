dd8484d7d4b8e7b6b4d52964923a1016ce0a4fe2
devxyn <johnlawrencehermo@gmail.com>
2025-03-29 00:37:55 +0800

    feat: add CORS middleware and update package dependencies in package.json and package-lock.json for enhanced API security and functionality

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import connectDB from './config/database.js';
 import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
@@ -9,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 dotenv.config();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
