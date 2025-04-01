44ba8f041911408c84b6cd9e4d5fd4186c36b067
devxyn <johnlawrencehermo@gmail.com>
2025-04-01 19:49:02 +0800

    chore: remove vite-env.d.ts file as type declarations for @vitejs/plugin-react-swc are no longer needed

--- client-v2/src/types/vite-env.d.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module '@vitejs/plugin-react-swc' {
-  import { Plugin } from 'vite';
-  export default function reactSWC(options?: any): Plugin;
-}
