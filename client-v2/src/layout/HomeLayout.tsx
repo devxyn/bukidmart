059290aeba04e8f490d457c6f726e02e50d26db3
devxyn <johnlawrencehermo@gmail.com>
2025-04-01 14:31:02 +0800

    feat: create HomeLayout component to structure main layout with Navbar and Outlet in client-v2

---
+++ client-v2/src/layout/HomeLayout.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/Navbar';
+import { Outlet } from 'react-router';
+
+const HomeLayout = () => {
+  return (
+    <div className='w-full h-screen'>
+      <Navbar />
+      <div className='w-full h-[80vh]'>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default HomeLayout;
