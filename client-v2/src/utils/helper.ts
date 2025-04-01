72e739712a6b049333f488ab162e8448541954f9
devxyn <johnlawrencehermo@gmail.com>
2025-04-02 00:30:43 +0800

    feat: remove client-v2 directory and all associated files including configuration, components, pages, and assets

--- client-v2/src/utils/helper.ts
+++
@@ -1,7 +0,0 @@
-import { toast } from 'react-toastify';
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const handleError = (error: any) => {
-  console.log(error);
-  toast.error(error?.response?.data?.message);
-};
