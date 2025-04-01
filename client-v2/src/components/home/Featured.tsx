a01606784ac7a05730601e73ae97dfefa56d1671
devxyn <johnlawrencehermo@gmail.com>
2025-04-01 17:41:32 +0800

    feat: implement Featured component to display featured products on the home page in client-v2

---
+++ client-v2/src/components/home/Featured.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { getRequest } from '@/services/requestService';
+import { setFeaturedProducts } from '@/store/slices/productSlice';
+import { RootState } from '@/store/store';
+import { handleError } from '@/utils/helper';
+import ProductCard from '@components/shared/ProductCard';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+const Featured = () => {
+  const dispatch = useDispatch();
+  const featuredProducts = useSelector((state: RootState) => state.product.featuredProducts);
+
+  const handleGetFeaturedProducts = async () => {
+    try {
+      const response = await getRequest('/products', { params: { featured: true } });
+      dispatch(setFeaturedProducts(response));
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetFeaturedProducts();
+  }, []);
+
+  return (
+    <section className='h-auto lg:h-screen flex flex-col items-center justify-center gap-10 lg:gap-20 px-5 py-10'>
+      <h2 className='text-3xl lg:text-5xl tracking-widest uppercase'>Featured Products</h2>
+      <div className='flex flex-wrap flex-col md:flex-row gap-5 justify-center items-center'>
+        {featuredProducts.map((product) => (
+          <ProductCard key={product._id} isLoading={false} product={product} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Featured;
