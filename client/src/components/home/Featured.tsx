51bc5edf04bba9d267a46df23fb64a83c27d9912
devxyn <johnlawrencehermo@gmail.com>
2025-04-02 01:10:38 +0800

    feat: enhance Featured component with loading state and fallback for empty product list

--- client/src/components/home/Featured.tsx
+++ client/src/components/home/Featured.tsx
@@ -4,19 +4,25 @@ import { setFeaturedProducts } from '@store/slices/productSlice';
 import { RootState } from '@store/store';
 import { handleError } from '@utils/helper';
 import ProductCard from '@components/shared/ProductCard';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Featured = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   const dispatch = useDispatch();
   const featuredProducts = useSelector((state: RootState) => state.product.featuredProducts);
+  const mockProducts = Array.from({ length: 4 }, () => ({}));
 
   const handleGetFeaturedProducts = async () => {
+    setIsLoading(true);
     try {
       const response = await getRequest('/products', { params: { featured: true } });
       dispatch(setFeaturedProducts(response));
     } catch (error) {
       handleError(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -28,9 +34,9 @@ const Featured = () => {
     <section className='h-auto lg:h-screen flex flex-col items-center justify-center gap-10 lg:gap-20 px-5 py-10'>
       <h2 className='text-3xl lg:text-5xl tracking-widest uppercase'>Featured Products</h2>
       <div className='flex flex-wrap flex-col md:flex-row gap-5 justify-center items-center'>
-        {featuredProducts.map((product) => (
-          <ProductCard key={product._id} isLoading={false} product={product} />
-        ))}
+        {featuredProducts && featuredProducts.length > 0
+          ? featuredProducts.map((product) => <ProductCard key={product._id} isLoading={isLoading} product={product} />)
+          : mockProducts.map((_, index) => <ProductCard key={index} product={null} />)}
       </div>
     </section>
   );
