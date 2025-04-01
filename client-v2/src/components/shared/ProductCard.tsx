fe924bd174814f6081e1ba71c5d9fa3924acd388
devxyn <johnlawrencehermo@gmail.com>
2025-04-01 17:42:01 +0800

    feat: create ProductCard component to display product details and loading state in client-v2

---
+++ client-v2/src/components/shared/ProductCard.tsx
@@ -0,0 +1,45 @@
+interface ProductData {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  stocks: number;
+  image: string;
+  category: string;
+  isFeatured: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface ProductCardProps {
+  isLoading: boolean;
+  product: ProductData;
+}
+
+const ProductCard = ({ isLoading = false, product }: ProductCardProps) => {
+  if (isLoading) {
+    return (
+      <div className='flex w-60 md:w-64 flex-col gap-4'>
+        <div className='skeleton h-52 w-full'></div>
+        <div className='skeleton h-4 w-3/4'></div>
+        <div className='skeleton h-4 w-1/2'></div>
+        <div className='skeleton h-4 w-1/2'></div>
+      </div>
+    );
+  }
+
+  return (
+    <div className='w-80 flex-shrink-0 md:flex-shrink relative'>
+      <p className='absolute top-3 right-4 bg-secondary text-white text-center text-xs font-bold tracking-wide px-3 py-2 rounded-xl'>
+        {product.category.toUpperCase()}
+      </p>
+      <img className='w-full' src={product.image} alt='coco-pandan' />
+      <div className='w-full md:w-auto overflow-x-auto'>
+        <h3 className='text-3xl font-semibold tracking-wide'>{product.name}</h3>
+        <p className='text-xl font-semibold text-secondary tracking-wide'>₱ {product.price.toLocaleString('en-US')}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
