c6b8f217390cb64e8c31e8b019ced9722f756992
devxyn <johnlawrencehermo@gmail.com>
2025-04-01 17:40:33 +0800

    feat: add Banner component for promotional display on the home page in client-v2

---
+++ client-v2/src/components/home/Banner.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router';
+import banner from '@assets/images/banner.png';
+
+const Banner = () => {
+  return (
+    <div className='bg-neutral h-screen lg:h-full px-5 pt-16 pb-10 lg:px-20 relative flex items-center justify-center overflow-hidden'>
+      <div className='text-left z-10'>
+        <h1 className='text-4xl lg:text-5xl font-semibold tracking-wide lg:tracking-widest leading-tight mb-10'>
+          <span className='text-secondary font-bold'>Farm-fresh</span> goodness delivered to your doorstep
+        </h1>
+        <div className='flex justify-center lg:justify-start gap-4'>
+          <Link to='/products' className='btn btn-accent w-[10rem] h-[3rem] text-white text-xl'>
+            Shop now
+          </Link>
+          <button className='btn btn-outline btn-primary w-[10rem] h-[3rem] text-xl'>Learn more</button>
+        </div>
+      </div>
+
+      <div className='lg:hidden bg-[#f3fcf7]/80 w-full h-full absolute top-0 left-0 z-[5]'></div>
+      <img
+        src={banner}
+        alt='overlay-banner'
+        className='w-full lg:w-[50%] h-full lg:h-auto absolute top-[100px] left-0 object-cover object-center z-[1] lg:static lg:z-auto'
+      />
+    </div>
+  );
+};
+
+export default Banner;
