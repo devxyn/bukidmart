2447afad09819e837cb18cfb835438676e05e80e
devxyn <johnlawrencehermo@gmail.com>
2025-04-01 17:42:25 +0800

    feat: add testimonials data with user feedback and images in client-v2

---
+++ client-v2/src/data/testimonials.ts
@@ -0,0 +1,27 @@
+import shiela from '@assets/images/shiela.jpg';
+import leny from '@assets/images/leny.jpg';
+import frio from '@assets/images/frio.jpg';
+
+export const testimonials = [
+  {
+    name: 'Shiela P.',
+    title: 'UI/UX Designer',
+    message:
+      "I've been a loyal customer of BukidMart for over a year now, and I can't imagine going back to buying groceries any other way. The freshness of their produce is unmatched, and the convenience of having it delivered to my doorstep saves me so much time. Plus, knowing that I'm supporting local farmers makes the experience even better!",
+    image: shiela,
+  },
+  {
+    name: 'Leny R.',
+    title: 'Civil Worker',
+    message:
+      'As someone who values healthy eating and supporting local agriculture, BukidMart has become my go-to for all my grocery needs. Their commitment to providing fresh, organic produce is evident in every order I receive. I love being able to trust where my food comes from, and BukidMart never disappoints.',
+    image: leny,
+  },
+  {
+    name: 'Joshua F.',
+    title: 'College Student',
+    message:
+      "BukidMart has truly transformed my shopping routine. The ease of receiving fresh farm produce at my door is unmatched. Knowing that I'm supporting local farmers while receiving exceptional customer service adds significant value to each purchase. BukidMart has earned my loyalty through their reliable service and commitment to quality.",
+    image: frio,
+  },
+];
