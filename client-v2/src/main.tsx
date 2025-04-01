222c57f28806f4f40410f685f93f801d36638ea9
devxyn <johnlawrencehermo@gmail.com>
2025-04-01 14:26:14 +0800

    feat: wrap App component with BrowserRouter and add ToastContainer for notifications in client-v2

--- client-v2/src/main.tsx
+++ client-v2/src/main.tsx
@@ -5,12 +5,29 @@ import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { persistor, store } from './store/store.ts';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Bounce, ToastContainer } from 'react-toastify';
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+        <ToastContainer
+          position='bottom-right'
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme='light'
+          transition={Bounce}
+        />
       </PersistGate>
     </Provider>
   </StrictMode>,
