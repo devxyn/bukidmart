9a7f7b67dbb331dcfbbcd7da35840b49201ffced
devxyn <johnlawrencehermo@gmail.com>
2025-04-01 18:22:20 +0800

    feat: update routing in App component to include Sign Up page for user registration

--- client-v2/src/App.tsx
+++ client-v2/src/App.tsx
@@ -2,7 +2,8 @@ import Login from '@/pages/auth/Login';
 import { Route, Routes } from 'react-router';
 import AuthLayout from '@layout/AuthLayout';
 import HomeLayout from '@layout/HomeLayout';
-import Home from './pages/home/Home';
+import Home from '@pages/home/Home';
+import SignUp from '@/pages/auth/SignUp';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path='/auth' element={<AuthLayout />}>
           <Route path='login' element={<Login />} />
+          <Route path='signup' element={<SignUp />} />
         </Route>
       </Route>
     </Routes>
