2cd07b05e840014cb8403d03064c64c9a307a02d
devxyn <johnlawrencehermo@gmail.com>
2025-04-01 18:55:50 +0800

    feat: serve static files and handle routing for production environment in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
+
 import connectDB from './config/database.js';
 import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
@@ -9,19 +11,25 @@ import cartRoutes from './routes/cartRoutes.js';
 const app = express();
 const port = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api', (req, res) => {
-  res.send('Server is running!');
-});
-
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client-v2/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client-v2', 'dist', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   connectDB();
