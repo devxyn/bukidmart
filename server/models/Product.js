c9771037b2e31aa655e3ca22329cff5e6c8bc981
devxyn <johnlawrencehermo@gmail.com>
2025-04-01 17:43:20 +0800

    feat: enhance product schema to include timestamps and maintain structure in Product model

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,18 +1,21 @@
 import { Schema, model } from 'mongoose';
 
-const productSchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  stocks: { type: Number, required: true, default: 0 },
-  image: { type: String, required: true, default: 'image.png' },
-  category: {
-    type: String,
-    enum: ['rice', 'vegetable', 'fruit'],
-    required: true,
+const productSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    stocks: { type: Number, required: true, default: 0 },
+    image: { type: String, required: true, default: 'image.png' },
+    category: {
+      type: String,
+      enum: ['rice', 'vegetable', 'fruit'],
+      required: true,
+    },
+    isFeatured: { type: Boolean, default: false },
   },
-  isFeatured: { type: Boolean, default: false },
-});
+  { timestamps: true },
+);
 
 const Product = model('Product', productSchema);
 
