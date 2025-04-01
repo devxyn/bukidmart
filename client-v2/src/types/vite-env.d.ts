0cf3f6bfd31cd4591759243d0f8ed2cfd4759d93
devxyn <johnlawrencehermo@gmail.com>
2025-04-01 19:39:10 +0800

    chore: add type definitions for @vitejs/plugin-react-swc in vite-env.d.ts

---
+++ client-v2/src/types/vite-env.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '@vitejs/plugin-react-swc' {
+  import { Plugin } from 'vite';
+  export default function reactSWC(options?: any): Plugin;
+}
