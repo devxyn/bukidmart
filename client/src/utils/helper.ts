3a113b64f6fe5c6aad6224cd73aa682312d4f026
devxyn <johnlawrencehermo@gmail.com>
2025-04-03 00:13:13 +0800

    fix: update error handling in helper function to display more relevant error messages

--- client/src/utils/helper.ts
+++ client/src/utils/helper.ts
@@ -3,5 +3,5 @@ import { toast } from 'react-toastify';
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const handleError = (error: any) => {
   console.log(error);
-  toast.error(error?.response?.data?.message);
+  toast.error(error?.message);
 };
