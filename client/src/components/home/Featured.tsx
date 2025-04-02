6a577728d0b3d4486e62a6f47581e3f86e1c8d6c
devxyn <johnlawrencehermo@gmail.com>
2025-04-03 01:37:48 +0800

    feat: enhance styling of Featured component header and improve ProductCard rendering logic

--- client/src/components/home/Featured.tsx
+++ client/src/components/home/Featured.tsx
@@ -32,11 +32,11 @@ const Featured = () => {
 
   return (
     <section className='h-auto lg:h-screen flex flex-col items-center justify-center gap-10 lg:gap-20 px-5 py-10'>
-      <h2 className='text-3xl lg:text-5xl tracking-widest uppercase'>Featured Products</h2>
+      <h2 className='text-3xl lg:text-5xl tracking-widest uppercase font-bold text-center'>Featured Products</h2>
       <div className='flex flex-wrap flex-col md:flex-row gap-5 justify-center items-center'>
         {featuredProducts && featuredProducts.length > 0
           ? featuredProducts.map((product) => <ProductCard key={product._id} isLoading={isLoading} product={product} />)
-          : mockProducts.map((_, index) => <ProductCard key={index} product={null} />)}
+          : mockProducts.map((_, index) => <ProductCard key={index} />)}
       </div>
     </section>
   );
