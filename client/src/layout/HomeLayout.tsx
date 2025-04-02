20767ed23527512ed9c4b6f6fa6d4869434cd559
devxyn <johnlawrencehermo@gmail.com>
2025-04-03 00:12:34 +0800

    fix: update HomeLayout component for improved layout structure and responsiveness

--- client/src/layout/HomeLayout.tsx
+++ client/src/layout/HomeLayout.tsx
@@ -4,9 +4,9 @@ import { Outlet } from 'react-router';
 
 const HomeLayout = () => {
   return (
-    <div className='w-full h-screen'>
+    <div className='w-full min-h-screen flex flex-col'>
       <Navbar />
-      <div className='flex flex-col w-full'>
+      <div className='flex flex-col flex-1'>
         <Outlet />
       </div>
       <Footer />
