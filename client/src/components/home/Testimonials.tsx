5f8f472ff70b46ce7a17ed2663a85d7d57abc380
devxyn <johnlawrencehermo@gmail.com>
2025-04-03 01:37:54 +0800

    feat: enhance styling of Testimonials component header for improved emphasis and readability

--- client/src/components/home/Testimonials.tsx
+++ client/src/components/home/Testimonials.tsx
@@ -4,7 +4,7 @@ import { testimonials } from '@data/testimonials';
 const Testimonials = () => {
   return (
     <section className='h-auto lg:h-screen flex flex-col items-center justify-center gap-10 lg:gap-20 px-5 py-10'>
-      <h2 className='text-3xl lg:text-5xl tracking-widest'>TESTIMONIALS</h2>
+      <h2 className='text-3xl lg:text-5xl tracking-widest uppercase font-bold text-center'>TESTIMONIALS</h2>
       <div className='flex flex-col lg:flex-row justify-center gap-2 lg:gap-10'>
         {testimonials.map((testimony) => (
           <TestimonialCard key={testimony.name} testimony={testimony} />
