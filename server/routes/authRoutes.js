3f398f12def82751f35ad320c4c4ba72503a17dd
devxyn <johnlawrencehermo@gmail.com>
2025-11-06 18:35:04 +0800

    feat: add validation middleware to auth routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { signUp, login, me } from '../controllers/authController.js';
-import { verifyToken } from '../middlewares/authMiddleware.js';
+import { validateLogin, validateSignUp, verifyToken } from '../middlewares/authMiddleware.js';
 
 const router = new Router();
 
-router.post('/signup', signUp);
-router.post('/login', login);
+router.post('/signup', validateSignUp, signUp);
+router.post('/login', validateLogin, login);
 router.get('/me', verifyToken, me);
 
 export default router;
