a5a6f6ef6b8bd27b76386a5fb5e21e52a796f433
devxyn <johnlawrencehermo@gmail.com>
2025-11-06 14:09:17 +0800

    delete: remove vercel server configuration

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import path from 'path';
-import { createServer } from '@vercel/node';
 
 import connectDB from './config/database.js';
 import authRoutes from './routes/authRoutes.js';
@@ -37,4 +36,3 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
 
-export default createServer(app);
